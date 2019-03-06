65e74b21ebd5d1402de02928b40e0f6f4894d501
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 15:28:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
 holiday.each do |supply|
-  puts 
+  puts "#{season}: #{holiday}: #{supply}"
 end 
 end
 end
