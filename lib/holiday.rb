64c0b878f7d2aeedc23ec528d434974d9686dbb8
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 14:54:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ bbq_hols = []
 holiday_hash.each do |season,holiday|
   holiday.each do |holiday, supply|
     if supply == "BBQ"
-      bbq_hols.push(hol)
+      bbq_hols.push(holiday)
     end 
   end 
 end
