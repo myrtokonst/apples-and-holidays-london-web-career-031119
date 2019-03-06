bd425417ca38ce2622f95136002c42c16605f904
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-06 15:31:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
+  cap_season= season.capitalize
 holiday.each do |supply|
   puts "#{season}: #{holiday}: #{supply}"
 end 
